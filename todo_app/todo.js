53718c2ebc1dabb5f698db9f5d31c1e29f727684
Danszjur <mr.danszjur@gmail.com>
2025-09-26 11:08:36 +0200

    Add files via upload

---
+++ todo_app/todo.js
@@ -0,0 +1,30 @@
+class Todos {
+    constructor(title) {
+        this.title = title;
+        this.description = null;
+        this.dueDate = new Date;
+        this.priority = 2;
+        this.status = false;
+
+        this.notes = "";
+        this.checklists = null;
+
+    }
+
+    staticsetTitle(newTitle) { this.title = newTitle }
+    getTitle() { return this.title }
+
+    setDescription(newDescription) { this.description = newDescription }
+    getDescription() { this.description }
+
+    setDueDate(newDueDate) { this.dueDate = newDueDate }
+    getDueDate() { return this.dueDate }
+
+    setPriority(newPriority) { this.priority = newPriority }
+    getPriority() { return this.priority }
+
+    setStatus(newStatus) { this.status = newStatus }
+    getStatus() { return this.status }
+}
+
+export default Todos
